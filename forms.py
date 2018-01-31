e14e3b8ae69378f87afbabf8832b57b987b3914c
Kyle <kvanderson102@gmail.com>
2018-01-30 18:23:01 -0600

    Updated schema for model and forms

--- forms.py
+++ forms.py
@@ -1,13 +1,14 @@
 from flask_wtf import FlaskForm
-from wtforms import TextField, PasswordField
+from wtforms import StringField, PasswordField
+from wtforms.validators import InputRequired, Email, Length
 
 
 class LoginForm(FlaskForm):
-    username = TextField('username')
-    password = PasswordField('password')
+    username = StringField('username', validators=[InputRequired(), Length(min=8, max=50)])
+    password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=50)])
 
 
 class SignupForm(FlaskForm):
-    # email = Email('email')
-    username = TextField('username')
+    email = Email('email')
+    username = StringField('username')
     password = PasswordField('password')
--- models.py
+++ models.py
@@ -3,5 +3,29 @@ from app import db
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50))
-    email = db.Column(db.String(50))
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    email = db.Column(db.String(50), unique=True, nullable=False)
+    habits = db.relationship('Habit', backref='user', lazy='dynamic')
+    settings = db.relationship('Settings', backref='user', uselist=False)
+
+
+class Settings(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    start_of_week = db.Column(db.Integer, default=1)
+    start_of_month = db.Column(db.Integer, default=0)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    # _user = db.relationship('User', back_populates="settings")
+
+
+class Habit(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    habit = db.Column(db.String(120), nullable=False)
+    dates = db.relationship('DateEvent', backref='habit', lazy='dynamic')
+
+
+class DateEvent(db.Model):
+    date = db.Column(db.Date, primary_key=True)
+    is_complete = db.Column(db.Boolean, nullable=False, default=0)
+    is_active = db.Column(db.Boolean, nullable=False, default=1)
+    habit_id = db.Column(db.Integer, db.ForeignKey('habit.id'))
